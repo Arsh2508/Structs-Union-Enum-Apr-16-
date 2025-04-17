c61b40ff523b245328a6ebccde1f3d310690fe8f
Arshak Zaqaryan <arshakzakaryan2004@gmail.com>
2025-04-18 03:34:46 +0400

    add problem8

--- problem7/main.c
+++ problem7/main.c
@@ -13,7 +13,7 @@ int main()
 	ob.b = 12.5;
 	printf("Float value %.2f\n", ob.b);
 	
-	printf("Size of union: %lu bytes\n", sizeof(ob.b)); // union have the size of the largest member on it
+	printf("Size of union: %lu bytes\n", sizeof(ob)); // union have the size of the largest member on it
 	return 0;
 	
 }
---
+++ problem8/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+union Data{
+	int a;
+	float b;
+	char c;
+};
+
+int main()
+{
+	union Data ob;
+	ob.a = 12;
+	printf("int: %d float: %f char: %c\n", ob.a, ob.b, ob.c);
+
+	ob.b = 34.5;
+	printf("int: %d float: %f char: %c\n", ob.a, ob.b, ob.c);
+
+	ob.c = 'A';
+	printf("int: %d float: %f char: %c\n", ob.a, ob.b, ob.c);
+	
+	printf("Size of union: %lu\n", sizeof(ob));
+	return 0;
+}
